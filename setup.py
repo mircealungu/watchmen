4e0a890aa243688a0ef988a24f049517437cf6da
Lukeslux <Lucas.brnd@gmail.com>
2017-05-18 14:42:01 +0200

    Added setup script.

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='watchmen',
+      version='0.1',
+      description='Simple python library to parse articles and feeds.',
+      maintainer='Zeeguu',
+      url='https://github.com/mircealungu/watchmen',
+      package_dir = {'': 'src'},
+      packages=['watchmen'],
+      )
